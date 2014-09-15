3f685161f091137dc4cd49db6e1588c84e40d22e
VarunJyoti <varun@miritech.com>
2014-09-14 22:33:37 -0700

    Destination Trip Fragment

--- app/src/main/java/org/ninja/tripgang/CreateTrip.java
+++ app/src/main/java/org/ninja/tripgang/CreateTrip.java
@@ -1,24 +1,44 @@
 package org.ninja.tripgang;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import org.ninja.tripgang.R;
 
-public class CreateTrip extends Activity implements TripType.OnFragmentInteractionListener {
+public class CreateTrip extends Activity implements TripType.OnFragmentInteractionListener, TripDestination.OnFragmentInteractionListener {
     String[] TripType = {"Camping", "Picnic", "Surfing", "Road Trip"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_trip);
+        final FragmentManager fm= getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        final Fragment tripType = (Fragment)fm.findFragmentById(R.id.trip_type_fragment);
+        final Fragment tripDest = (Fragment)fm.findFragmentById(R.id.trip_type_destination);
+        ft.hide(tripDest);
+        ft.commit();
         ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_type_triplist_view,TripType);
         ListView listview = (ListView) findViewById(R.id.trip_type);
         listview.setAdapter(adapter);
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.show(tripDest);
+                ft.hide(tripType);
+                ft.commit();
+            }
+        });
     }
 
     @Override
