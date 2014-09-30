e8e1e1f3aed8176977b6707007116a97a98a1900
Hemant Dhiman <hemantdhiman@gmail.com>
2014-09-29 22:15:41 -0700

    Enable Google Login

--- app/src/main/java/org/ninja/tripgang/LoginActivity.java
+++ app/src/main/java/org/ninja/tripgang/LoginActivity.java
@@ -2,20 +2,51 @@ package org.ninja.tripgang;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.support.v7.app.ActionBarActivity;
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
+import com.facebook.HttpMethod;
+import com.facebook.Request;
+import com.facebook.Response;
 import com.facebook.Session;
 import com.facebook.SessionState;
 import com.facebook.UiLifecycleHelper;
+import com.facebook.model.GraphUser;
+import com.facebook.widget.LoginButton;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.plus.Plus;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.plus.model.people.Person;
+
+import java.util.Arrays;
+import java.util.List;
 
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends Activity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
     private Context context = this;
     private boolean isResumed = false;
+
+    private static final int RC_SIGN_IN = 0;
+
+    private GoogleApiClient mGoogleApiClient;
+
+    private boolean mIntentInProgress;
+
+    private boolean mSignInClicked;
+
+    private ConnectionResult mConnectionResult;
+
+
     private UiLifecycleHelper uiHelper;
     private Session.StatusCallback callback =
             new Session.StatusCallback() {
@@ -28,10 +59,46 @@ public class LoginActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Plus.API)
+                .addScope(Plus.SCOPE_PLUS_LOGIN)
+                .build();
+
         uiHelper = new UiLifecycleHelper(this,callback);
+
+
         setContentView(R.layout.activity_login);
+        final SignInButton googleButton =  (SignInButton) findViewById(R.id.google_sign_in_button);
+        googleButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                onClickGoogleLogin(v);
+            }
+        });
+        final LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList("user_friends","user_likes","user_status"));
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                onClickLogin();
+            }
+        });
 
     }
+
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    protected void onStop() {
+        super.onStop();
+
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -39,6 +106,7 @@ public class LoginActivity extends Activity {
         getMenuInflater().inflate(R.menu.login, menu);
         return true;
     }
+
 
     @Override
     public void onResume(){
@@ -57,6 +125,18 @@ public class LoginActivity extends Activity {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         uiHelper.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode != RESULT_OK) {
+                mSignInClicked = false;
+            }
+
+            mIntentInProgress = false;
+
+            if (!mGoogleApiClient.isConnecting()) {
+                mGoogleApiClient.connect();
+            }
+        }
+
     }
 
 
@@ -84,23 +164,100 @@ public class LoginActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void onConnectionSuspended(int cause) {
+        mGoogleApiClient.connect();
+    }
+
+
+    private void resolveSignInError() {
+        if (mConnectionResult.hasResolution()) {
+            try {
+                mIntentInProgress = true;
+                startIntentSenderForResult(mConnectionResult.getResolution().getIntentSender(),
+                        RC_SIGN_IN, null, 0, 0, 0);
+            } catch (IntentSender.SendIntentException e) {
+                // The intent was canceled before it was sent.  Return to the default
+                // state and attempt to connect to get an updated ConnectionResult.
+                mIntentInProgress = false;
+                mGoogleApiClient.connect();
+            }
+        }else{
+            GooglePlayServicesUtil.showErrorDialogFragment(mConnectionResult.getErrorCode(),this,RC_SIGN_IN);
+        }
+    }
+
+    public void onConnectionFailed(ConnectionResult result) {
+        if (!mIntentInProgress) {
+            // Store the ConnectionResult so that we can use it later when the user clicks
+            // 'sign-in'.
+            mConnectionResult =  result;
+
+            if (mSignInClicked) {
+                // The user has already clicked 'sign-in' so we attempt to resolve all
+                // errors until the user is signed in, or they cancel.
+                resolveSignInError();
+            }
+        }
+
+    }
+
+    public void onConnected(Bundle connectionHint) {
+        // We've resolved any connection errors.  mGoogleApiClient can be used to
+        // access Google APIs on behalf of the user.
+        mSignInClicked = false;
+        getUserInformation();
+
+    }
+
+    private void getUserInformation(){
+        if(Plus.PeopleApi.getCurrentPerson(mGoogleApiClient) != null){
+            Person currentPerson = Plus.PeopleApi.getCurrentPerson(mGoogleApiClient);
+            String personName = currentPerson.getDisplayName();
+            String personPhotoUrl = currentPerson.getImage().getUrl();
+            String personGooglePlusProfile = currentPerson.getUrl();
+            String email = Plus.AccountApi.getAccountName(mGoogleApiClient);
+            Log.e("TAG", "Name: " + personName + ", plusProfile: "
+                    + personGooglePlusProfile + ", email: " + email
+                    + ", Image: " + personPhotoUrl);
+            Intent intent = new Intent(context,Tripslist.class);
+            startActivity(intent);
+        }
+
+    }
 
     private void onSessionStateChange(Session session, SessionState state, Exception exception) {
 
-            if(isResumed) {
-                if (state.isOpened()) {
-                    // If the session state is open:
-                    // Show the authenticated fragment
-                    System.out.println("test");
-                    Intent intent = new Intent(context,Tripslist.class);
-                    startActivity(intent);
-                } else if (state.isClosed()) {
-                    // If the session state is closed:
-                    // Show the login fragment
-                    setContentView(R.layout.activity_login);
+        if(isResumed) {
+            if (state.isOpened()) {
+                // If the session state is open:
+                // Show the authenticated fragment
+                System.out.println("test");
+                Intent intent = new Intent(context,Tripslist.class);
+                startActivity(intent);
+            } else if (state.isClosed()) {
+                // If the session state is closed:
+                // Show the login fragment
+                setContentView(R.layout.activity_login);
 
-                }
             }
+        }
+
+    }
+    public void onClickLogin() {
+        Session session = Session.getActiveSession();
+        if (!session.isOpened() && !session.isClosed()) {
+            session.openForRead(new Session.OpenRequest(this)
+                    .setPermissions(Arrays.asList("user_friends","user_likes","user_status"))
+                    .setCallback(callback));
+        }
+    }
+
+    public void onClickGoogleLogin(View view){
+        if (view.getId() == R.id.google_sign_in_button
+                && !mGoogleApiClient.isConnecting()) {
+            mSignInClicked = true;
+            resolveSignInError();
+        }
 
     }
 }
